2ff30baf50f716de85b319dead47a7e2aafb95cd
Karol Dzialowski <karlososhd@gmail.com>
2020-12-21 16:46:26 +0100

    :alembic: epochs plot

--- 01_mlp/experiments_analysis.py
+++ 01_mlp/experiments_analysis.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-if __name__ == "__main__":
+def optimizers_activations_comparison():
+    # Porównanie wszystkich klasyfikatorów
     df = pd.read_csv("./experiments/experiment_1_100_epochs.csv")
 
     # Best models for 100 epochs
@@ -11,3 +13,22 @@ if __name__ == "__main__":
     print(df.groupby('optimizer').mean())
     print(df.groupby('lr').mean())
     print(df.groupby('activation').mean())
+
+
+def epochs_lr_comparison():
+    # Wpływ liczby epok na dokładność i loss function
+    df = pd.read_csv("./experiments/epochs_experiment_0.01.csv", index_col="epoch")
+    df.plot()
+    plt.title("Learning rate = 0.01")
+    plt.show()
+    print(df)
+
+    df = pd.read_csv("./experiments/epochs_experiment_0.10.csv", index_col="epoch")
+    df.plot()
+    plt.title("Learning rate = 0.10")
+    plt.show()
+    print(df)
+
+
+if __name__ == "__main__":
+    epochs_lr_comparison()
